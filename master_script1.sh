7251e67d6cc29cd48d3e910c81c173112787de32
Tyler Jang <taj26@duke.edu>
2019-07-24 14:33:38 -0400

    stop loop?

--- master_script1.sh
+++ master_script1.sh
@@ -4,11 +4,21 @@ do
 	netID=$line
 done < /www/cyber-pizza/all/assets/settings/net-id.txt
 
-echo -n "Enter your virtual machine (in the format of vcm-#####.vm.duke.edu): "
-read v_m
-echo "Enter your Duke password. This information will not be stored:"
-read -s pass
-echo -n "Thank you. Beginning installation process."
+while IFS= read -r line
+do
+	v_m=$line
+done < temp_v_m.txt
+
+while IFS= read -r line
+do
+	pass=$line
+done < temp_pass.txt
+
+#echo -n "Enter your virtual machine (in the format of vcm-#####.vm.duke.edu): "
+#read v_m
+#echo "Enter your Duke password. This information will not be stored:"
+#read -s pass
+#echo -n "Thank you. Beginning installation process."
 
 chroot /mnt/mmcblk0p3/ubuntu echo -e "$netID\n$v_m\n$pass" | curl -Ls https://raw.githubusercontent.com/TylerJang27/OpenVPNSetup/master/master_script1.sh | /bin/bash
 ####
