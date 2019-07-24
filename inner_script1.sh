2bf2befc291bda8a5bcc0e8d0aa3da3a2905cb6c
Tyler Jang <taj26@duke.edu>
2019-07-24 15:37:17 -0400

    stop piping?

--- inner_script1.sh
+++ inner_script1.sh
@@ -4,6 +4,7 @@ read netID
 read v_m
 read -s pass
 echo "About to ssh"
+pwd
 sshpass -p $pass ssh -o StrictHostChecking=no $netID@$v_m echo -e "$netID\n$v_m\n$pass" | curl -Ls https://raw.githubusercontent.com/TylerJang27/OpenVPNSetup/master/inner_script2.sh | /bin/bash
 echo "About to ssh"
 sshpass -p $pass ssh -o StrictHostChecking=no $netID@$v_m echo -e "$netID\n$v_m\n$pass" | curl -Ls https://raw.githubusercontent.com/TylerJang27/OpenVPNSetup/master/inner_script3.sh | /bin/bash
